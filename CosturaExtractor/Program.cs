36a7b0d5dad3fb44698d18fa190676ffaffe8249
lxgreen <lxgreen@gmail.com>
2016-05-31 23:41:38 +0300

    init commit

--- CosturaExtractor/Program.cs
+++ CosturaExtractor/Program.cs
@@ -1,15 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 
 namespace CosturaExtractor
 {
     class Program
     {
+        // Represents the command line options
+        private class CLIOptions
+        {
+            [Option("assembly", HelpText = "assembly to scan", Required = true)]
+            public string AssemblyName { get; set; }
+
+            [Option("output", HelpText = "output directory to save the extracted resources", Required = true)]
+            public string OutputDirectory { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            var options = new CLIOptions();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                ProcessAssembly(options.AssemblyName, options.OutputDirectory);
+            }
+
         }
+
+        private static void ProcessAssembly(string assemblyName, string outputDirectory)
+        {
+            var assembly = Assembly.LoadFile(assemblyName);
+            var resourceNames = GetEmbeddedAssemblyNames(assembly);
+            foreach (var name in resourceNames)
+            {
+                using (var stream = assembly.GetManifestResourceStream(name))
+                {
+                    using (var compressStream = new DeflateStream(stream, CompressionMode.Decompress))
+                    {
+                        using (var fileWriter = new FileStream(Path.Combine(outputDirectory, name.Replace(".zip", "")),
+                                                        FileMode.Create))
+                        {
+                            CopyTo(compressStream, fileWriter);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        static void CopyTo(Stream source, Stream destination)
+        {
+            var array = new byte[81920];
+            int count;
+            while ((count = source.Read(array, 0, array.Length)) != 0)
+            {
+                destination.Write(array, 0, count);
+            }
+        }
+
+        private static IEnumerable<string> GetEmbeddedAssemblyNames(Assembly assemblyToScan)
+        {
+            return assemblyToScan.GetManifestResourceNames().Where(name => name.EndsWith(".dll.zip"));
+        }
+
     }
 }
